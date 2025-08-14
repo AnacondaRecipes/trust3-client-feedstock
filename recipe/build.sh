e920dcd412dfaa99a5f3befd3790341347c530eb
Andrii Osipov <aosipov@anaconda.com>
2025-08-14 05:18:42 -0700

    fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,5 @@ set -xe
 # install using pip from the whl files on PyPI
 WHL_FILE=trust3_client-${PKG_VERSION}-py3-none-any.whl 
 curl -Lso "$WHL_FILE" https://pypi.org/packages/py3/t/trust3_client/trust3_client-${PKG_VERSION}-py3-none-any.whl
-#curl -Lso "$WHL_FILE" https://files.pythonhosted.org/packages/d2/17/399e4394cd6b2bdf130a03da680a4acfc6a49a7f63e8657f23fcca688f19/trust3_client-1.0.0-py3-none-any.whl
 
 $PYTHON -m pip install --no-deps --no-build-isolation -vvv $WHL_FILE
